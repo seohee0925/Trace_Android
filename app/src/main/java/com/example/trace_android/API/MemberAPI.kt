2c320ab065ce602ab73c372591d44c44c7b2954a
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-08 23:12:34 +0900

    Feat : 마이페이지 반영

--- app/src/main/java/com/example/trace_android/API/MemberAPI.kt
+++ app/src/main/java/com/example/trace_android/API/MemberAPI.kt
@@ -3,9 +3,14 @@ package com.example.trace_android.API
 import com.example.trace_android.model.Member
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface MemberAPI {
     @POST("/members/register")
     fun save(@Body member: Member): Call<Member>
+
+    @GET("/members/{memberEmail}")
+    fun getMemberByEmail(@Path("memberEmail") memberEmail: String): Call<Member>
 }
