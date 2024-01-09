0cc00a54aaa5a1d9cadb9958441ca7d1db16e849
JyngJ <kaijjy@kaist.ac.kr>
2024-01-10 03:41:32 +0900

    feature: post #10 ,, 텍스트, 사진 불러오기 성공

--- app/src/main/java/com/example/trace_android/PostDetailsFragment.kt
+++ app/src/main/java/com/example/trace_android/PostDetailsFragment.kt
@@ -1,14 +1,19 @@
 package com.example.trace_android
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.example.trace_android.model.Post
+
 
 
 class PostDetailsDialogFragment : DialogFragment() {
@@ -31,10 +36,26 @@ class PostDetailsDialogFragment : DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<TextView>(R.id.postViewContents).text = post?.content
 
+        val imageView = view.findViewById<ImageView>(R.id.postViewImage)
+        post?.let {
+            if (!it.image.isNullOrEmpty() || !it.imageExtra.isNullOrEmpty()) {
+                val combinedImageString = it.image + (it.imageExtra ?: "")
+                val imageBitmap = base64ToBitmap(combinedImageString)
+                imageView.setImageBitmap(imageBitmap)
+                imageView.visibility = View.VISIBLE
+            }
+        }
+
         // 취소 버튼 클릭 리스너 설정
         view.findViewById<Button>(R.id.buttonCancel).setOnClickListener {
             dismiss() // 프래그먼트 닫기
         }
+    }
+
+    //base64 to image
+    private fun base64ToBitmap(base64Str: String): Bitmap {
+        val imageBytes = Base64.decode(base64Str, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
