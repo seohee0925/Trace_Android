8a29dc8b6b4e717f1c66bf9d196986ac1bbcf291
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-09 19:25:22 +0900

    Feat : 마이페이지 사진 업로드 구현

--- app/src/main/java/com/example/trace_android/PostAdapter.kt
+++ app/src/main/java/com/example/trace_android/PostAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.trace_android
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class PostAdapter(private val items: ArrayList<PostData>) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,15 +27,24 @@ class PostAdapter(private val items: ArrayList<PostData>) : RecyclerView.Adapter
         return items.size
     }
 
+
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         private var view: View = v
         val contentTextView: TextView = v.findViewById(R.id.content)
         val placeTextView: TextView = v.findViewById(R.id.place)
-        // val imageView: ImageView = v.findViewById(R.id.contentImage)
+        val dateTextView: TextView = v.findViewById(R.id.date)
+        val imageView: ImageView = v.findViewById(R.id.contentImage)
 
         fun bind(item: PostData) {
             contentTextView.text = item.content
             placeTextView.text = item.place
+            dateTextView.text = item.date
+
+            // base64 문자열을 디코딩하여 이미지뷰에 설정
+            val decodedImageBytes: ByteArray = android.util.Base64.decode(item.combinedImage, android.util.Base64.DEFAULT)
+            val decodedBitmap: Bitmap? = BitmapFactory.decodeByteArray(decodedImageBytes, 0, decodedImageBytes.size)
+
+            imageView.setImageBitmap(decodedBitmap)
         }
     }
 }
